36c666f83fc615f6bc9e2db52a069f78f1038fc3
Todd W Crone <twcrone@gmail.com>
2015-12-03 15:11:50 -0500

    Step 8: Write Unit Tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,7 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-    ];
+    $scope.todos = [];
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
